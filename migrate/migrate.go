a5f720d3f4173720bfe43174e8143bb05063dae2
xerofenix <xerofenix@duck.com>
2024-12-23 19:28:08 +0530

    add migrate folder

---
+++ migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"crud_json/initializers"
+	"crud_json/models"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Post{})
+}
